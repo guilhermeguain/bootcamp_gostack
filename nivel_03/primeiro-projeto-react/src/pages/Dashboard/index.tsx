2d21097fbc6e8cbb34557e3ce29e74a198141669
Guilherme Guain <guilhermeguain@gmail.com>
2020-11-10 19:20:47 -0300

    level 03 - day 10 - first project with react.js

---
+++ nivel_03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/72861606?s=460&u=2b4976d08167fba4e709c4f7db8889c53a393542&v=4"
+            alt="Guilherme Guain"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/72861606?s=460&u=2b4976d08167fba4e709c4f7db8889c53a393542&v=4"
+            alt="Guilherme Guain"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/72861606?s=460&u=2b4976d08167fba4e709c4f7db8889c53a393542&v=4"
+            alt="Guilherme Guain"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
