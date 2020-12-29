8d0853eb625ce4643b19f6e473b93d57d2d4611a
Guilherme Guain <guilhermeguain@gmail.com>
2020-12-29 09:25:43 -0300

    level 04 - day 21 - continuing app backend

---
+++ nivel_04/continuando-backend-do-app/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,11 @@
+import FakeUserTokensRepository from '@modules/users/repositories/fakes/FakeUserTokensRepository';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
+    return template;
+  }
+}
+
+export default FakeMailTemplateProvider;
